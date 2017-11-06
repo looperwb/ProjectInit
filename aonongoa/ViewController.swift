51865f6c05e18181340e032f37799f5ea90c91ca
looperwb <looperwb@gmail.com>
2017-11-06 09:46:15 +0800

    Initial Commit

---
+++ aonongoa/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  aonongoa
+//
+//  Created by WB Zhou on 2017/11/6.
+//  Copyright © 2017年 zwb. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
